64fced6cc2a8fb64e2efa5b1cf5d39a7f2b2bec4
Fabiano Fazan <fabianofazan.softensistemas@gmail.com>
2025-09-26 07:34:56 -0300

    Ajustado o application.properties e DadosListagemMedico.java

--- api/src/main/java/med/voll/api/dto/DadosListagemMedico.java
+++ api/src/main/java/med/voll/api/dto/DadosListagemMedico.java
@@ -2,7 +2,9 @@ package med.voll.api.dto;
 
 import med.voll.api.entity.EspecialidadeMedica;
 import med.voll.api.entity.MedicoEntity;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public record DadosListagemMedico(String nome, String email, String crm, EspecialidadeMedica especialidadeMedica) {
 
 
